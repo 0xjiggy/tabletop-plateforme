ce1cc8dc2c2d695b5be6ceffe200dd88b6f34ff9
Ghassan Amaimia <ghassan.amaimia@tek-up.de>
2026-02-26 10:42:56 +0000

    feat(auth): add CreateUserDto

---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  IsNotEmpty,
+  IsEnum,
+  IsOptional,
+} from 'class-validator';
+import { UserRole } from '../../common/enums/user-role.enum';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsEnum(UserRole)
+  @IsOptional()
+  role?: UserRole;
+}
